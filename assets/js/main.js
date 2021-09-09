1e40f5548f32e5425255126926a26f65f8438d2e
Administrator <imani@iran.ir>
2021-09-09 12:59:49 +0430

    wip: search res api

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,23 @@ function dev() {
   console.log("salam")
 }
 
-function seach() {
+async function seach() {
   console.log("salam")
+  let keyword = 'news'
+  let query = `/q/${ keyword }/nonce/12`
+  let response = await fetch('http://localhost:3000' + query)
+  response.json()
 }
 
 function eventListener() {
   document.getElementById("btn_search").onclick = x=>{
     seach()
+      .then( data => {
+        console.log(data)
+      })
+      .catch( err => {
+        console.error(err)
+      })
     document.getElementById('search_page').classList.add("hide")
     document.getElementById('result_page').classList.remove("hide")
   }
